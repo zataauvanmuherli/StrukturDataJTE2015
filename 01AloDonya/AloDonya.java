e8b69ae2c6c0a4992f2f9910df6bccbf3c6f86a9
zataauvanmuherli <zataopan99@yahoo.com>
2015-09-05 17:16:38 +0700

    StrukturDataJTE2015 Tugas 01AloDonya

---
+++ 01AloDonya/AloDonya.java
@@ -0,0 +1,16 @@
+
+/**
+ * Write a description of class AloDonya here.
+ * 
+ * @author (your name) 
+ * @version (a version number or a date)
+ */
+public class AloDonya
+{ 
+    public  static void main (String[]Args)
+    {
+        System.out.println("AloDonya") ;
+    }
+}
+    
+ 
